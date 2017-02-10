67bfd74b019b164f6e5811e6da20960f9822b343
MayankHolmes <mainhoonrakhwala@gmail.com>
2017-02-10 17:17:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var content={
+    title:Mayank HOlmes
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
