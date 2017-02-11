2eb62ec13874a04884c7b025fb3fdcd926c309cb
MayankHolmes <mainhoonrakhwala@gmail.com>
2017-02-11 15:56:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var days={
-     'Day2':{
+     Day2:{
    title:'Mayank Holmes',
    content:`<p>
          Welcome to NPTEL.
@@ -51,7 +51,7 @@ app.get('/', function (req, res) {
 
 app.get('/:DayDigit', function (req, res) {
     var dayDigit=req.params.DayDigit;
-  res.send(createtemp(days[DayDigit]));
+  res.send(createtemp(Day2));
 });
 
 app.get('/ui/style.css', function (req, res) {
