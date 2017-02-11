e06971b5da1dfa1874661d3839da6998fa844b05
MayankHolmes <mainhoonrakhwala@gmail.com>
2017-02-11 15:03:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,43 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var may={
+   title:'Mayank Holmes',
+   content:`<p>
+         Welcome to NPTEL.
+     </p>`
+   
+};
+
+function createtemp(data)
+{
+    var title=data.title;
+    var content=data.content;
+var htmltemp=
+   ` <!DOCTYPE html>
+<head>
+    <title>
+        ${title}
+    </title>
+
+<link href="/ui/style.css" rel="stylesheet" />
 
 
-
+</head>
+<body>
+    <a 
+    href='/'>home</a>
+<div class="container"> <div>
+     ${content}
+ </div> 
+ </div>
+    <p>
+        HELLO.HOWDIE.
+    </p>
+</body>`
+;
+return htmltemp;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
