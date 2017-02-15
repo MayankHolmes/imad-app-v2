d4357ca56da913c17d34560abeed003a711bc4ce
MayankHolmes <mainhoonrakhwala@gmail.com>
2017-02-15 21:29:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter + 1;
+  res.send(counter.toString());
+});
+
 app.get('/:DayDigit', function (req, res) {
     var DayDigit=req.params.DayDigit;
   res.send(createtemp(days[DayDigit]));
