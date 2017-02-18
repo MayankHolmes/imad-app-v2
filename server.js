4dd0f28fd9b71767a3b0ee3dc7a5df1777fc6735
MayankHolmes <mainhoonrakhwala@gmail.com>
2017-02-18 23:06:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('ui/12196277_1651812745097614_3209256683447267969_n.pngui/12196277_1651812745097614_3209256683447267969_n.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'ui/12196277_1651812745097614_3209256683447267969_n.png'));
 });
 
 
