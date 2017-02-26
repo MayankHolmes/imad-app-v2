0bb09e153fdd7db2407269f086e6cc125f311a3e
MayankHolmes <mainhoonrakhwala@gmail.com>
2017-02-26 17:51:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,6 @@ console.log('Loaded!');
 app.get('ui/12196277_1651812745097614_3209256683447267969_n.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'ui/12196277_1651812745097614_3209256683447267969_n.png'));
 });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
