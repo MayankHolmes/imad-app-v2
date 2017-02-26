160054a9ceabf09097592681bf1836069040eb58
MayankHolmes <mainhoonrakhwala@gmail.com>
2017-02-26 17:52:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,7 +45,9 @@ var htmltemp=
 ;
 return htmltemp;
 }
-
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 var counter=0;
 app.get('/counter', function (req, res) {
