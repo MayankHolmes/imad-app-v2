629385577375fbe4879406ae5e2f59e2fd5c08ed
MayankHolmes <mainhoonrakhwala@gmail.com>
2017-03-05 11:18:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,18 @@ button.onclick= function(){
   var span=document.getElementById('count');
   span.innerHTML=counter.toString();
 };
+var input=document.getElementById('comment');
+var comment =input.value;
+var submit=document.getElementById('submit_btn');
+
+submit.onclick=function(){
+    var comments=['1','2','3'];
+     var list='';
+     for(var i=0;i<name.length();i++)
+     {
+         list <- '<li>'+comments[i]+'</li>';
+     }
+     
+     var ul=document.getElementById('submit_btn');
+     ul.innerHTML=list;
+};
